a70798bb86b9665d10acc66e22a3a9d5ad715e3e
Lucas de Azambuja <lucasazsantos@gmail.com>
2020-09-02 10:02:37 -0300

    single page app part2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
@@ -39,6 +40,24 @@ class Main extends Component {
                 />
             );
         };
+
+        const DishWithId = ({ match }) => {
+            return (
+                <DishDetail
+                    dish={
+                        this.state.dishes.filter(
+                            (dish) =>
+                                dish.id === parseInt(match.params.dishId, 10)
+                        )[0]
+                    }
+                    comments={this.state.comments.filter(
+                        (comment) =>
+                            comment.dishId === parseInt(match.params.dishId, 10)
+                    )}
+                />
+            );
+        };
+
         return (
             <div>
                 <Header />
@@ -49,6 +68,7 @@ class Main extends Component {
                         path='/menu'
                         component={() => <Menu dishes={this.state.dishes} />}
                     />
+                    <Route path='/Menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
